34e690ca7e8291b828beb603a3a96a7d7bb1a6fd
Max Base <Max@Asrez.com>
2020-02-23 17:45:56 +0330

    Update json-reader.php

--- json-reader.php
+++ json-reader.php
@@ -4,6 +4,7 @@
 $dataFile=file_get_contents("output.json");
 $data=json_decode($dataFile, true);
 // print_r($data);
+/*
 readJson($data);
 
 function readJson($data) {
@@ -17,3 +18,42 @@ function readJson($data) {
 		}
 	}
 }
+*/
+
+$persianFile=file_get_contents("names-fa.txt");
+$persianLines=explode("\n", $persianFile);
+$englishFile=file_get_contents("names-en.txt");
+$englishLines=explode("\n", $englishFile);
+readJson($data);
+
+function findWord($fa) {
+	global $persianLines;
+	global $englishLines;
+	// print_r($persianLines);
+	// print_r($englishLines);
+	// exit();
+	foreach($persianLines as $i=>$line) {
+		if($line == $fa) {
+			return $englishLines[$i];
+		}
+	}
+	return $fa;
+}
+
+function readJson($data) {
+	global $dataFile;
+	// file_put_contents("names-fa.txt", "");
+	foreach($data as $item) {
+		if(isset($item["name"])) {
+			// file_put_contents("names-fa.txt", $item["name"]."\n", FILE_APPEND);
+			$enW=findWord($item["name"]);
+			// $dataFile=str_replace(utf8_encode($item["name"]), utf8_encode($enW), $dataFile);
+			$dataFile=str_replace(($item["name"]), ($enW), $dataFile);
+			print $item["name"]." => " . $enW . "\n";
+		}
+		if(isset($item["children"])) {
+			readJson($item["children"]);
+		}
+	}
+}
+file_put_contents("output-en.json", $dataFile);
