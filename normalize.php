bb5b78d80ff6a63d4607bd3cce7a52eabb6b1a2b
Max Base <Max@Asrez.com>
2020-02-23 15:30:45 +0330

    Update normalize.php

--- normalize.php
+++ normalize.php
@@ -6,5 +6,6 @@ $data=strip_tags($data,"<ul><li><b><h1><h2>");
 $data=html_entity_decode($data);
 $data=htmlspecialchars_decode($data);
 $data=str_replace("هٔ", "ه", $data);
+$data=trim($data);
 file_put_contents("input-normal.html", $data);
 print $data;
