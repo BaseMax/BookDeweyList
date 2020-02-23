0f5354c26a72257a5100b7da820db479c437807a
Max Base <Max@Asrez.com>
2020-02-23 16:14:44 +0330

    Update parser.php

--- parser.php
+++ parser.php
@@ -1,3 +1,7 @@
 <?php
 // Max Base
 // https://github.com/BaseMax/BookDeweyList
+$data=file_get_contents("input-normal.html");
+$pattern='/<!-- begin -->\n<h2>([^\<]+)<\/h2>[\n\s]+<ul>(.*?)<\/ul>\n<!-- end -->/is';
+preg_match_all($pattern, $data, $output);
+print_r($output);
