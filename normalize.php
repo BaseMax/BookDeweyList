50a8387c4fca584b809634b87dcd214f76adae0b
Max Base <Max@Asrez.com>
2020-02-23 15:25:47 +0330

    Update normalize.php

--- normalize.php
+++ normalize.php
@@ -1,3 +1,9 @@
 <?php
 // Max Base
 // https://github.com/BaseMax/BookDeweyList
+$data=file_get_contents("input.html");
+$data=strip_tags($data,"<ul><li><b><h1><h2>");
+$data=html_entity_decode($data);
+$data=htmlspecialchars_decode($data);
+file_put_contents("input-normal.html", $data);
+print $data;
